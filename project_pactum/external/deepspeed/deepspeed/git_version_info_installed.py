0dcfb52b950d630b4ec1893b0549e4f8f796472e
gaoziyuan <gaoziyuan19@mails.ucas.ac.cn>
2024-03-05 19:47:43 +0800

    try sync versions

--- project_pactum/external/deepspeed/deepspeed/git_version_info_installed.py
+++ project_pactum/external/deepspeed/deepspeed/git_version_info_installed.py
@@ -1,6 +1,6 @@
-version='0.5.2+4c7a31c'
-git_hash='4c7a31c'
+version='0.5.2+0803f89'
+git_hash='0803f89'
 git_branch='main'
 installed_ops={'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'async_io': False, 'transformer_inference': False, 'utils': False, 'quantizer': False}
-compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'async_io': False, 'transformer_inference': True, 'utils': True, 'quantizer': True}
-torch_info={'version': '1.10', 'cuda_version': '0.0'}
+compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': True, 'transformer': True, 'stochastic_transformer': True, 'async_io': False, 'transformer_inference': True, 'utils': True, 'quantizer': True}
+torch_info={'version': '1.13', 'cuda_version': '11.7'}
