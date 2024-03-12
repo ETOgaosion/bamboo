838508a6da7b939f87afc48fde534c30dfbcd62f
gaoziyuan <gaoziyuan19@mails.ucas.ac.cn>
2024-03-13 00:49:18 +0800

    try to fix steps per run bug, forgot to pull

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-MODEL=${1}
+WAY=${1}
+MODEL=${2}
 
-python -m project_pactum.simulation --generate-table --model $MODEL
+if [ $WAY -eq 1 ]; then
+    python -m project_pactum.simulation --generate-table --model $MODEL
+else
+    python -m project_pactum.simulation --generate-graphs --model $MODEL
+fi
