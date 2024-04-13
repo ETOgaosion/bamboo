811db535455f069ca2a14cfaefb15621325b0f5a
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-04-13 19:15:48 +0800

    add simulate pure total graph

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python -m project_pactum.simulation --generate-graphs --spot-instance-trace traces/p3-trace.csv --model GPT-2 --fig-directory res/simuitest > simu.txt 2>&1
+# python -m project_pactum.simulation --generate-graphs --spot-instance-trace traces/p3-trace.csv --model GPT-2 --fig-directory res/simuitest > simu.txt 2>&1
+
+python -m project_pactum.simulation --generate-graphs --model GPT-2 --fig-directory res/simuitest-no-trace > simu.txt 2>&1
