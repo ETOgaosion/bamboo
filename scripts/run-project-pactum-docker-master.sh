8d7cbb21e069866098e080196eae58fb248d0a84
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-05-20 15:30:37 +0800

    rename

---
+++ scripts/run-project-pactum-docker-master.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CUR_NODE=${1:-0}
+
+if [ $CUR_NODE -eq 0 ]; then
+    echo "Running on master node"
+    docker build -t whatcanyousee/bamboo:latest .
+    etcdctl rm --dir --recursive /torchelastic
+fi
+
+cmd="""docker run -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
+    /bin/bash -c './scripts/run-project-pactum.sh' > res/lab/node_$CUR_NODE.txt 2>&1"""
+
+# cmd="""docker run --rm -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
+#     /bin/bash -c './scripts/run-project-pactum.sh'"""
+
+echo "RUNNING CMD $cmd"
+
+eval $cmd
