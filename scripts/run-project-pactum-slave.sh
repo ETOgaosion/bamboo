d09671892c83af7524e7d87f757c169e443bc7ec
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-05-21 10:21:59 +0800

    to see whether gpt3 can run

--- scripts/run-project-pactum-slave.sh
+++ scripts/run-project-pactum-slave.sh
@@ -13,8 +13,6 @@ MODEL=${CURRENT_PATH}/project_pactum/external/deepspeed/DeepSpeedExamples/pipeli
 echo "ARGS $RDZV_IP $ID $NUM_STAGES $MODEL"
 
 cmd="""export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
-	export NCCL_DEBUG=INFO \
-	export NCCL_DEBUG_SUBSYS=ALL \
 	export USE_BARRIER=true \
 	&& \
 	export PYTHONPATH=${CURRENT_PATH}/project-pactum:\${PYTHONPATH} \
