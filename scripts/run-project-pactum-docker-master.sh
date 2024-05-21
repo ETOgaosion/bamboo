67a46f60328240e93a36ea728f1e689a1e88ae97
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-05-21 17:06:47 +0800

    update output path

--- scripts/run-project-pactum-docker-master.sh
+++ scripts/run-project-pactum-docker-master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CUR_NODE=${1:-0}
+NUM_NODES=${2:-16}
+NUM_STAGES=${3:-16}
 
 if [ $CUR_NODE -eq 0 ]; then
     echo "Running on master node"
@@ -8,11 +10,13 @@ if [ $CUR_NODE -eq 0 ]; then
     etcdctl rm --dir --recursive /torchelastic
 fi
 
+mkdir -p res/lab/nodes_$NUM_NODES
+
 cmd="""docker run -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-    /bin/bash -c './scripts/run-project-pactum-master.sh' > res/lab/node_$CUR_NODE.txt 2>&1"""
+    /bin/bash -c './scripts/run-project-pactum-master.sh $NUM_NODES $NUM_STAGES' > res/lab/nodes_$NUM_NODE/node_$CUR_NODE.txt 2>&1"""
 
 # cmd="""docker run --rm -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-#     /bin/bash -c './scripts/run-project-pactum-master.sh'"""
+#     /bin/bash -c './scripts/run-project-pactum-master.sh $NUM_NODES $NUM_STAGES'"""
 
 echo "RUNNING CMD $cmd"
 
--- scripts/run-project-pactum-docker-slave.sh
+++ scripts/run-project-pactum-docker-slave.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 CUR_NODE=${1:-0}
+NUM_NODES=${2:-16}
+NUM_STAGES=${3:-16}
 
 if [ $CUR_NODE -eq 0 ]; then
     echo "Running on master node"
     docker build -t whatcanyousee/bamboo:latest .
 fi
 
+mkdir -p res/lab/nodes_$NUM_NODES
+
 cmd="""docker run -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-    /bin/bash -c './scripts/run-project-pactum-slave.sh' > res/lab/node_$CUR_NODE.txt 2>&1"""
+    /bin/bash -c './scripts/run-project-pactum-slave.sh $NUM_NODES $NUM_STAGES' > res/lab/nodes_$NUM_NODE/node_$CUR_NODE.txt 2>&1"""
 
 # cmd="""docker run --rm -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-#     /bin/bash -c './scripts/run-project-pactum-slave.sh'"""
+#     /bin/bash -c './scripts/run-project-pactum-slave.sh $NUM_NODES $NUM_STAGES'"""
 
 echo "RUNNING CMD $cmd"
 
