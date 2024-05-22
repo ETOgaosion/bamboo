b24ff35e42b82d8472004350f017a0a6b78c9348
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-05-22 15:00:52 +0800

    decrease iteration number

--- scripts/run-project-pactum-slave.sh
+++ scripts/run-project-pactum-slave.sh
@@ -27,7 +27,7 @@ cmd="""export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
 	--max-pipe-parallel-size=24 \
 	--default-num-stages=${NUM_STAGES} \
 	${MODEL}.py \
-	-s 10 \
+	-s 5 \
 	--backend=nccl \
 	--redundancy_level=1 \
 	${@:5} \
