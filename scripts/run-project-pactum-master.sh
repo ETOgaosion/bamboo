dc17e0b9adaa6782b807bb66f264cfc0378f8895
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-06-11 15:17:28 +0800

    can set rank by arguments

--- scripts/run-project-pactum-master.sh
+++ scripts/run-project-pactum-master.sh
@@ -15,6 +15,7 @@ echo "ARGS $RDZV_IP $ID $NUM_STAGES $GLOBAL_RANK $MODEL"
 cmd="""export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
 	export NCCL_SOCKET_IFNAME=eno1 \
 	export USE_BARRIER=true \
+	export GLOBAL_RANK=$GLOBAL_RANK \
 	export PYTHONPATH=${CURRENT_PATH}/project-pactum:\${PYTHONPATH} && \
 	python -m project_pactum.run \
 	--rdzv_backend=etcd-v2 \
