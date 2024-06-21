c1abf46445c568dfb1f99fe9e5bd7f3588e8f992
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-06-21 14:48:59 +0800

    try to run on aws

--- scripts/run-project-pactum-docker-slave-pssh.sh
+++ scripts/run-project-pactum-docker-slave-pssh.sh
@@ -10,20 +10,19 @@ LAYERS=${7:-24}
 # REBUILD=${6}
 
 # if [ $CUR_NODE -eq 0 ]; then
-#     echo "Running on master node"
+#     echo "Running on slave node"
 #     if [ $REBUILD -eq 1 ]; then
 #         docker build -t whatcanyousee/bamboo:latest .
 #     fi
-#     # etcdctl rm --dir --recursive /torchelastic
 # fi
 
 mkdir -p "res/lab/nodes_$NUM_NODES_$NUM_STAGES"
 
-cmd="""docker run --rm -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-    /bin/bash -c './scripts/run-project-pactum-master.sh $NUM_NODES $NUM_STAGES $GLOBAL_RANK $MICRO_BATCH_SIZE $SEQ_LEN $LAYERS' > res/lab/nodes_$NUM_NODES_$NUM_STAGES/node_$CUR_NODE.txt 2>&1"""
+cmd="""docker run --rm --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
+    /bin/bash -c './scripts/run-project-pactum-slave.sh $NUM_NODES $NUM_STAGES $GLOBAL_RANK $MICRO_BATCH_SIZE $SEQ_LEN $LAYERS' > res/lab/nodes_$NUM_NODES_$NUM_STAGES/node_$CUR_NODE.txt 2>&1"""
 
 # cmd="""docker run --rm -it --net "host" --gpus 'device=$CUR_NODE' -w '/workspace' whatcanyousee/bamboo \
-#     /bin/bash -c './scripts/run-project-pactum-master.sh $NUM_NODES $NUM_STAGES'"""
+#     /bin/bash -c './scripts/run-project-pactum-slave.sh $NUM_NODES $NUM_STAGES'"""
 
 echo "RUNNING CMD $cmd"
 
